1017e519f641619efd6a24525e0a5310ab4bee7e
SophieShawntel <boundtomakeit210@gmail.com>
2024-09-23 01:35:33 -0700

    update

--- Backend/routes.py
+++ Backend/routes.py
@@ -19,7 +19,7 @@ def create_friend():
         required_fields = ["name", "role", "description", "gender"]
         for field in required_fields:
             if field not in data:
-                return jsonify({"error": f"missing required field: {field}. Please add"}), 400
+                return jsonify({"error": f"missing required field: {field}. Please aaaadd"}), 400
 
 
 
@@ -48,23 +48,46 @@ def create_friend():
         db.session.rollback()
         return jsonify({"error":str(e)}), 500
 
-                                    #DELETE A FRIEND
+                                    #DELETE A FRIEND  QST:  is this method applicable to a web app that has many user.
 
 
 @app.route("/api/friends/<int:id>", methods=['DELETE']) 
 def delete_friend(id):
     try:
-        friend = friend.query.get(id)
-        if friend is None:
+        friends = friend.query.get(id)                  ## used 'FRIENDS' as variable here becuase 'FRIEND' was having issues. Maybe because of confusing in flask or python have same Class/Model name as variable
+        if friends is None:
             return jsonify({"error":"Friend Not Found"}), 404
 
-        db.session.delete(friend)
+        db.session.delete(friends)
         db.session.commit()
         return jsonify({"msg":"Friend Deleted"}), 200
     except Exception as e:
         db.session.rollback() 
-        return jsonify({"error":str(e)}), 500                                
+        return jsonify({"error":str(e)}), 500     
 
+
+                                         #UPDATE A FRIEND
+                                                          
+@app.route("/api/friends/<int:id>", methods=['PATCH'])
+def update_friend(id):
+    try:
+        friend = friend.query.get(id)                   # Varaible name 'FRIEND' did not disturb here 
+        if friend is None:
+            return jsonify({"error":"Friends Not Found"}), 404
+        
+        data = request.get_json()                           
+                                                            #Now remember In the approach below, you're using data.get("field", default_value), which simplifies the logic by allowing you to update only the fields that are provided, while keeping the existing values if a field is not included in the request.
+        friend.name = data.get("name", friend.name)          
+        friend.role = data.get("role", friend.role)
+        friend.description = data.get("description", friend.description)
+        friend.gender = data.get("gender", friend.gender)
+
+        db.session.commit()
+        
+        return jsonify(friend.to_json()), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error":str(e)}), 500
                         
          
 
